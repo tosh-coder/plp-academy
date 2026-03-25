49e965cafd47b4e2a6455bfda620bcaebea56ec5
Agent12 <petermeshack245@gmail.com>
2026-03-25 18:59:58 +0300

    python calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+#Ask the user to input two digits.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#Output Computed Results
+print(f"Sum: {num1 + num2}")
+print(f"Difference: {num1 - num2}")
+print(f"Product: {num1 * num2}")
+print(f"Quotient: {num1 / num2}")
